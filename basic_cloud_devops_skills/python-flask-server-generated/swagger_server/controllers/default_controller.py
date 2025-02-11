d7b16d035826a4c4557769db59eab4668db1e39c
sejproo <c.sejwani@student.vu.nl>
2025-02-11 18:33:06 +0100

    database logic implemented

--- basic_cloud_devops_skills/python-flask-server-generated/swagger_server/controllers/default_controller.py
+++ basic_cloud_devops_skills/python-flask-server-generated/swagger_server/controllers/default_controller.py
@@ -1,3 +1,5 @@
+from swagger_server.service.student_service import *
+
 import connexion
 import six
 
@@ -17,7 +19,8 @@ def add_student(body=None):  # noqa: E501
     """
     if connexion.request.is_json:
         body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        return add(body)
+    return 500, 'error'
 
 
 def delete_student(student_id):  # noqa: E501
@@ -30,7 +33,9 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    if(student_id != None):
+        return delete(student_id)
+    return 500,'error'
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -43,4 +48,6 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
+    if(student_id != None):
+        return get_by_id(student_id)
     return 'do some magic!'
